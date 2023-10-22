d85247992d901dbe2390ded6e7980a95dacbe2c6
DNI9 <itsr9boi@gmail.com>
2023-10-22 16:07:44 +0530

    setup tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1>Santosh Money Manager</h1>;
+  return (
+    <div className="bg-yellow-400 text-cyan-300 h-screen w-screen flex justify-center items-center">
+      <h1 className="text-9xl">Santosh Money Manager</h1>
+    </div>
+  );
 }
 
 export default App;
