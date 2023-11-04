f47b22276478068b7e1ec65394998f9c6db170b9
DNI9 <itsr9boi@gmail.com>
2023-11-04 16:50:45 +0530

    setup basepath

--- vite.config.ts
+++ vite.config.ts
@@ -1,47 +1,50 @@
-import react from '@vitejs/plugin-react';
-import {defineConfig} from 'vite';
-import {VitePWA} from 'vite-plugin-pwa';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
-      injectRegister: 'auto',
+      registerType: "autoUpdate",
+      injectRegister: "auto",
       devOptions: {
         enabled: true,
       },
       manifest: {
-        name: 'PWA App',
-        short_name: '',
-        description: 'Allows you to pin custom text to your notifications',
-        theme_color: '#14b8a5',
+        name: "PWA App",
+        short_name: "",
+        description: "Allows you to pin custom text to your notifications",
+        theme_color: "#14b8a5",
         icons: [
           {
-            src: 'pwa-64x64.png',
-            sizes: '64x64',
-            type: 'image/png',
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
           },
           {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any',
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
           },
           {
-            src: 'maskable-icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable',
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
           },
         ],
       },
     }),
   ],
+  resolve: {
+    alias: [{ find: "@", replacement: "/src" }],
+  },
 });
