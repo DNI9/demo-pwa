265352a924bac3084d7734095f1c89d7d561553c
DNI9 <itsr9boi@gmail.com>
2023-11-04 16:52:40 +0530

    setup initial routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 import "./globals.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Layout from "./layout";
-import Home from "./routes/Home";
-import About from "./routes/About";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/about",
-        element: <About />,
-      },
-    ],
-  },
-]);
+import { router } from "./routes";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
